26f53ffe1ec0dbda7b33a8ce50cd13f30a4dcfcc
tejaravutla287 <tejaravutla287@gmail.com>
2026-07-01 12:16:23 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  backend "azurerm" {} # Configured dynamically in DevOps pipeline
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Free Tier App Service Plan (Windows-based for easy static/HTML or .NET testing)
+resource "azurerm_service_plan" "asp" {
+  name                = var.app_service_plan_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Windows"
+  sku_name            = "F1" # F1 is the completely FREE tier
+}
+
+resource "azurerm_windows_web_app" "app" {
+  name                = var.app_service_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {
+    always_on = false # Required to be false for Free Tier
+  }
+
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.app_insights.instrumentation_key
+  }
+}
+
+resource "azurerm_application_insights" "app_insights" {
+  name                = "insights-poc17"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
